bbae34bf3fed630529246f0a04e2886566db6f9b
zhiltsov-max <zhiltsov.max35@gmail.com>
2018-10-21 12:15:55 +0300

    Changed confusion matrix to use ints for histogram

--- nn_compression/utils/confusion_matrix.py
+++ nn_compression/utils/confusion_matrix.py
@@ -1,4 +1,5 @@
-import torch
+import numpy as np
+
 
 class ConfusionMatrix:
     """
@@ -15,15 +16,15 @@ class ConfusionMatrix:
         self.classes = classes[:]
         self.class_count = len(classes)
         self.check_args = check_args
-        self.confusion = torch.Tensor(self.class_count, self.class_count)
+        self.confusion = np.zeros((self.class_count, self.class_count))
 
     def check_segmentation(self, image_array):
-        has_invalid_values = (torch.any(image_array < 0).item()) or \
-                             (torch.any(image_array >= self.class_count).item())
+        has_invalid_values = (np.any(image_array < 0)) or \
+                             (np.any(image_array >= self.class_count))
         if has_invalid_values:
             raise Exception(
                 'Array contains unexpected values beyond interval [0; %d]: %s.' \
-                % (self.class_count - 1, torch.unique(image_array)))
+                % (self.class_count - 1, np.unique(image_array)))
 
     def update(self, output, target):
         """
@@ -44,53 +45,54 @@ class ConfusionMatrix:
 
         if output.dim() != 1:
             output = output.view(self.class_count, -1).argmax(dim=0)
-        target = target.view(-1).to(dtype=torch.float, device='cpu')
+        output = output.cpu().numpy()
+        target = target.view(-1).cpu().numpy()
 
-        if output.size() != target.size():
+        if output.size != target.size:
             raise Exception("Array sizes are different: %s, %s" % \
-                (output.size(), target.size()))
+                (output.size, target.size))
 
         if self.check_args:
             self.check_segmentation(output)
             self.check_segmentation(target)
 
         if self.sumim is None:
-            self.sumim = torch.tensor(output, dtype=torch.float, device='cpu')
+            self.sumim = np.array(output, dtype=int)
         sumim = self.sumim
 
         sumim[:] = output
         sumim *= self.class_count
         sumim += target
-        confusion += sumim[target < 255].histc(
-            bins=confusion.numel(), min=0, max=confusion.numel()) \
-            .view_as(confusion)
+        hist, _ = np.histogram(sumim[target < 255],
+            bins=range(0, confusion.size + 1))
+        confusion += hist.reshape(confusion.shape)
 
         self.dirty = True
 
     def compute_overall_accuracy(self):
         self.overall_acc = 100.0 * \
-            torch.sum(torch.diag(self.confusion)) / torch.sum(self.confusion)
+            np.sum(np.diagonal(self.confusion)) / np.sum(self.confusion)
         return self.overall_acc
 
     def compute_class_accuracies(self):
-        self.class_acc = torch.zeros(self.class_count)
-        denoms = torch.sum(self.confusion, dim=0)
+        self.class_acc = np.zeros(self.class_count)
+        denoms = np.sum(self.confusion, axis=0)
         for i in range(self.class_count):
             if (denoms[i] == 0):
                 denoms[i] = 1
             self.class_acc[i] = 100.0 * self.confusion[i][i] / denoms[i]
-        self.avg_class_acc = torch.sum(self.class_acc) / self.class_count
+        self.avg_class_acc = np.sum(self.class_acc) / self.class_count
         return self.class_acc, self.avg_class_acc
 
     def compute_class_IoU(self):
-        self.class_iou = torch.zeros(self.class_count)
-        gtj = torch.sum(self.confusion, dim=1)
-        resj = torch.sum(self.confusion, dim=0)
+        self.class_iou = np.zeros(self.class_count)
+        gtj = np.sum(self.confusion, axis=1)
+        resj = np.sum(self.confusion, axis=0)
         for j in range(self.class_count):
             gtjresj = self.confusion[j][j]
             denom = max(gtj[j] + resj[j] - gtjresj, 1.0)
             self.class_iou[j] = 100.0 * gtjresj / denom
-        self.avg_class_iou = torch.sum(self.class_iou) / self.class_count
+        self.avg_class_iou = np.sum(self.class_iou) / self.class_count
         return self.class_iou, self.avg_class_iou
 
     def update_results(self):
